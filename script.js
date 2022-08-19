c77cb4d4d76afa0857138f9ae7558fa7d84323f4
olena <liabraolena@gmail.com>
2022-08-19 23:36:07 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// alert('I am Java Script!');
+// 
+// let admin;
+// let name;
+// name = 'John';
+// admin = name;
+// alert(admin);
+// //
+// let Earth;
+// let currentUser;
+// //
+// const BIRTHDAY = '18.04.1982'; 
+// const age = someCode(BIRTHDAY); 
+//
+let userName = prompt("Input your name", ""); //потрібне дефолтне значення для IE   
+alert(`Hello, ${userName} !`);
+
