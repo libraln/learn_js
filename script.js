4573db09389f1d3dc4b75401f9e6135f2cb9430d
olena <liabraolena@gmail.com>
2022-08-20 12:58:00 +0300

    variables

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-// alert('I am Java Script!');
-// 
-// let admin;
-// let name;
-// name = 'John';
-// admin = name;
-// alert(admin);
-// //
-// let Earth;
-// let currentUser;
-// //
-// const BIRTHDAY = '18.04.1982'; 
-// const age = someCode(BIRTHDAY); 
+let admin;
+let name;
+name = 'John';
+admin = name;
+alert(admin);
 //
-let userName = prompt("Input your name", ""); //потрібне дефолтне значення для IE   
-alert(`Hello, ${userName} !`);
+let Earth;
+let currentUser;
+//
+const BIRTHDAY = '18.04.1982'; 
+const age = someCode(BIRTHDAY); 
+
 
