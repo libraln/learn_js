5674051a13e37efe3853d6730325502048c43145
olena <liabraolena@gmail.com>
2022-08-20 22:39:45 +0300

    conditional operator

---
+++ numbers.js
@@ -0,0 +1,6 @@
+let number = prompt('input number', 0);
+if( number < 0 ) {
+    alert(-1);
+} else if( number > 0 ) {
+    alert(1);
+} else alert(0);
--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-let userName = prompt("Input your name", ""); //потрібне дефолтне значення для IE   
-alert(`Hello, ${userName} !`);
+let JSName = prompt('What is the “official” name of JavaScript?', '');
+if(JSName == 'ECMAScript') {
+    alert('True');
+} else {
+    alert('You don`t know? ECMAScript!')
+}
