7ea33b992787a3b9424f4ea957b3d565b3619478
olena <liabraolena@gmail.com>
2022-08-21 18:29:19 +0300

    loops

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-let age;
-//в діапазоні
-if(age >= 14 && age <= 90);
-//не в діапазоні
-if(age < 14 || age > 90);
+//вивести парні числа
+for(let i = 2; i <= 10; i++) {
+    if( i % 2 == 0){
+        alert(i);
+    } 
+}
+// //замінити for на while
+for (let i = 0; i < 3; i++) {
+    alert( `число ${i}!` );
+  }
 
-if(!(age >=14 && age <=90));
+let i = 0;
+while( i < 3 ) {
+    alert( `число ${i}!` );
+    i++;
+}
+//повторити цикл, поки не буде числа більше 100
+let num = 0;
+while( num < 100 || num ==='' || num === null){ 
+    num = prompt('input number > 100', 0);
+}
+
+//вивести прості числа
+let n = prompt('input num', 0);
+label:
+for(let i = 2; i <= n; i++) {
+    for (let j = 2; j < i; j++) {
+        if(i % j == 0){
+            continue label;
+        } 
+    }
+    alert(i);
+}
 
