c3c46509ad9a0c2e474f879099656cef05c0e086
olena <liabraolena@gmail.com>
2022-08-28 18:14:41 +0300

    numbers

--- script.js
+++ script.js
@@ -1,61 +1,19 @@
-let user = {};
-user.name = "Ivan";
-user.surname = "Smith";
-user.name = "Petro";
-delete user.name;
+let a = +prompt('input num 1', 0);
+let b = +prompt('input num 2', 0);
+let sum = a + b;
+console.log(sum);
 
+function readNumber() {
+  let num;
+  do {
+    num = prompt('input num', 0);
 
-//перевірка чи об'єкт пустий
-
-function isEmpty(object) {
-  for (key in object) {
-    return false;
+  }while(!isFinite(num))
+  if(num == null || num == ''){
+    return null;
   }
-  return true;
+  return +num;
 }
-
-//
-let salaries = {
-  John: 100,
-  Ann: 160,
-  Pete: 130
-}
-
-let sum = 0;
-
-function calcSum (obj) {
-  for ( let key in obj) {
-    sum += obj[key]; 
-  }
-  return sum;
-}
-
-let result = calcSum(salaries);
-console.log(result);
-//
-let menu = {
-  width: 200,
-  height: 300,
-  title: "Моє меню"
-};
-
-let res = multiplyNumeric(menu);
-console.log(menu);
-
-menu = {
-  width: 400,
-  height: 600,
-  title: "Моє меню"
-};
-
-function multiplyNumeric(obj) {
-  for (let key in obj) {
-    if (typeof obj[key] !== "number") {
-      continue;
-    }
-    obj[key] *= 2;
-  }
-}
-
+console.log(readNumber());
 
 
