6ca13c34da485c3b557bd992697167fbf3fbf6af
olena <liabraolena@gmail.com>
2022-08-30 13:35:52 +0300

    string

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
-let a = +prompt('input num 1', 0);
-let b = +prompt('input num 2', 0);
-let sum = a + b;
-console.log(sum);
-
-function readNumber() {
-  let num;
-  do {
-    num = prompt('input num', 0);
-
-  }while(!isFinite(num))
-  if(num == null || num == ''){
-    return null;
-  }
-  return +num;
+function ucFirst(name) {
+  let res = name[0].toUpperCase() + name.slice(1);
+  console.log(res); 
+  return res;
 }
-console.log(readNumber());
+ucFirst("василь");
 
+//
 
+function checkSpam(str) {
+  let strLowerCase = str.toLowerCase();
+  if(strLowerCase.includes('viagra') || strLowerCase.includes('xxx')) {
+    return true;
+  }
+  return false;
+}
+
+console.log(checkSpam('buy ViAgRA now'));
+console.log(checkSpam('free xxxxx'));
+console.log(checkSpam("innocent rabbit"));
+
+//
+
+function truncate(str, maxlength) {
+  if (str.length <= maxlength){
+    return str;
+  }
+  let slicedStr = str.slice(0, maxlength - 1) + '...';
+  return slicedStr;
+}
+console.log(truncate("Що я хотів би розповісти на цю тему:", 20)); 
+console.log(truncate("Всім привіт!", 20));
+
+//
+function extractCurrencyValue(str) {
+  let money = +str.slice(1);
+  return money;
+}
+
+console.log(extractCurrencyValue('$120'));
