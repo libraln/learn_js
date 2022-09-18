5cbd8e679bbb6d8934d8b96cfbcb60d782d4fdfe
olena <liabraolena@gmail.com>
2022-09-18 21:57:50 +0300

    json

--- getWeekDay.js
+++
@@ -1,9 +0,0 @@
-let date = new Date(2012, 01, 20, 3, 12);
-alert(date);
-
-function getWeekDay(date) {
-    let days = ['НД', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
-    return days[date.getDay()];
-}
-let date = new Date(2012, 0, 3);  // 3 січня 2012
-alert( getWeekDay(date) );        // повинно виводити "ВТ"
---
+++ script.js
@@ -0,0 +1,33 @@
+let user = {
+    name: "Іван Іванов",
+    age: 35
+  };
+  let userJson = JSON.stringify(user);
+  console.log(userJson);
+  console.log(JSON.parse(userJson));
+
+  let room = {
+    number: 23
+  };
+  
+  let meetup = {
+    title: "Конференція",
+    occupiedBy: [{name: "Іван"}, {name: "Аліса"}],
+    place: room
+  };
+  
+  // циклічне посилання
+  room.occupiedBy = meetup;
+  meetup.self = meetup;
+  
+  alert( JSON.stringify(meetup, function replacer(key, value) {
+    return (key != "" && value== meetup) ? undefined : value;
+  }));
+  
+  /* результат повинен бути:
+  {
+    "title":"Конференція",
+    "occupiedBy":[{"name":"Іван"},{"name":"Аліса"}],
+    "place":{"number":23}
+  }
+  */
