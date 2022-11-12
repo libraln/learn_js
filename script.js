3a1c2277f1351e54989af31684e4fa74c48956d6
olena <liabraolena@gmail.com>
2022-11-12 13:45:31 +0200

    xhr

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
-function printNumbers(from, to) {
-  let i = from;
-  let intervalId = setInterval( function() {
-    alert(i);
-    if( i== to ) {
-      clearInterval(intervalId);
+const url = 'https://jsonplaceholder.typicode.com/users';
+function sendRequest (method, url, body = null) {
+  return new Promise( (res, rej) => {
+    const request = new XMLHttpRequest();
+    request.open(method, url);
+    request.responseType = 'json';
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.onload = () => {
+      if (request.status >= 400) {
+        rej(request.response);
+      } else {
+      res(request.response);
+      }
     }
-  i++ }, 1000);
+    request.onerror = () => {
+      rej(request.response);
+    }
+    request.send(JSON.stringify(body));
+  })
 }
 
-printNumbers(1,10);
+sendRequest('GET', url)
+.then(data => console.log(data))
+.catch(err => console.log(err))
 
-function printNums( from, to ) {
-  let i = from;
-  let timerId = setTimeout( function go() {
-    alert(i);
-    if( i < to ) {
-      setTimeout( go, 1000 )
-    }
-    i++;
-  }, 1000)
+const body = {
+  name: 'Helen',
+  age: 27
 }
-printNums(5,10);
+
+sendRequest('POST', url, body )
+.then(data => console.log(data))
+.catch(err => console.log(err))
